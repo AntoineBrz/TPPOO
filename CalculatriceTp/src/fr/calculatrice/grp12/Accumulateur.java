913ba3d2dfd119b387f877d031693f906bbeeed6
clarktsiory <65616064+clarktsiory@users.noreply.github.com>
2020-11-13 16:35:13 +0100

    Documentation Accumulateur

--- CalculatriceTp/src/fr/calculatrice/grp12/Accumulateur.java
+++ CalculatriceTp/src/fr/calculatrice/grp12/Accumulateur.java
@@ -6,14 +6,23 @@ import java.beans.PropertyChangeListener;
 
 public class Accumulateur implements IAccumulateur {
 
-	public static final String SAISIE = "saisie";
+	// Noms d'évènements à passer au contrôleur
+	public static final String SAISIE = "saisieEnCours";
 	public static final String PUSH = "nouveauNombre";
 	public static final String RESULTAT = "nouveauResultatDeCalcul";
+	
+	// Mémoire (stack) pour piocher les opérandes/opérateurs de calcul
 	Pile pile = new Pile();
+	// Objet qui notifie l'observeur (le contrôleur enregistré au constructeur)
 	PropertyChangeSupport pcs = new PropertyChangeSupport(this);
-	ArrayList<Character> memoireCharacter = new ArrayList<>();
+	// Mémoire accumulant les chiffres d'une saisie en cours
+	ArrayList<Character> memoireCaractere = new ArrayList<>(); 
 	
 	public Accumulateur(PropertyChangeListener propertyChangeListener) {
+		/*
+		 * Constructeur qui enregistre un observeur de type PropertyChangeListener,
+		 * qui va être notifié des types d'évènements définis dans les constantes de classe.
+		 */
 		this.pcs.addPropertyChangeListener(propertyChangeListener);
 	}
 	
@@ -26,19 +35,30 @@ public class Accumulateur implements IAccumulateur {
 
 	@Override
 	public void drop() {
+		/*
+		 * Même méthode que clear()
+		 */
 		pile.clear();
 	}
 
 	@Override
 	public void swap() {
+		/*
+		 * Interchange les deux derniers éléments,
+		 * utile pour les opérations - et / non commutatives
+		 */
 		pile.swap();
 	}
 
 	
-	/*CALCUL*/
+	/*CALCULS*/
 	
 	public void add() {
+		/*
+		 * Additionne les deux derniers nombres de la pile.
+		 */
 		Double result = (double)pile.pop() + (double)pile.pop();
+		// ancien nombre saisi qui est maintenant au sommet de la pile:
 		Double oldResult = this.pile.lastElement();
 		pile.add(result);
 		pcs.firePropertyChange(RESULTAT, oldResult, result);
@@ -47,6 +67,10 @@ public class Accumulateur implements IAccumulateur {
 
 	@Override
 	public void sub() {
+		/*
+		 * Soustrait les deux derniers nombres de la pile 
+		 * (avant-dernier - dernier).
+		 */
 		pile.swap();
 		Double result = - (double)pile.pop() + (double)pile.pop();
 		Double oldResult = this.pile.lastElement();
@@ -56,6 +80,9 @@ public class Accumulateur implements IAccumulateur {
 
 	@Override
 	public void mult() {
+		/*
+		 * Multiplie les deux derniers nombres de la pile.
+		 */
 		Double result = (double)pile.pop() * (double)pile.pop();
 		Double oldResult = this.pile.lastElement();
 		pile.add(result);
@@ -64,6 +91,10 @@ public class Accumulateur implements IAccumulateur {
 
 	@Override
 	public void div() {
+		/*
+		 * Fait le rapport des deux derniers nombres de la pile 
+		 * (avant-dernier / dernier).
+		 */
 		pile.swap();
 		Double result = (double)pile.pop() / (double)pile.pop();
 		Double oldResult = this.pile.lastElement();
@@ -73,6 +104,9 @@ public class Accumulateur implements IAccumulateur {
 
 	@Override
 	public void neg() {
+		/*
+		 * Renvoie l'opposé du dernier élément de la pile.
+		 */
 		Double oldResult=this.pile.pop();;
 		Double result=-oldResult;
 		pile.add(result);
@@ -81,8 +115,11 @@ public class Accumulateur implements IAccumulateur {
 
 	@Override
 	public void backspace() {
+		/*
+		 * Annule la saisie du dernier chiffre tapé sur la calculatrice.
+		 */
 		try {
-			memoireCharacter.remove(memoireCharacter.size()-1);
+			memoireCaractere.remove(memoireCaractere.size()-1);
 		} catch(Exception e) {
 			
 		}
@@ -91,9 +128,13 @@ public class Accumulateur implements IAccumulateur {
 
 	@Override
 	public void accumuler(Character touche) {
-		memoireCharacter.add(touche);
+		/*
+		 * Cumule les chiffres (de 0 à 9) en cours de saisie
+		 * et envoie le nombre correspondant au cumul actuel au contrôleur.
+		 */
+		memoireCaractere.add(touche);
 		String cumul = "";
-		for(Character chiffre:memoireCharacter)
+		for(Character chiffre:memoireCaractere)
 			cumul += chiffre;
 		pcs.firePropertyChange(SAISIE, null, Double.parseDouble(cumul));
 	}
@@ -103,16 +144,19 @@ public class Accumulateur implements IAccumulateur {
 	@Override
 	public void reset() {
 		/*
-		 * Appui sur entrée
+		 * Récupère le cumul de chiffre fait par accumuler()
+		 * qui correspond au nombre entier saisi.
+		 * Envoie ce nombre entier au contrôleur 
+		 * pour notifier de la fin de saisie.
 		 */
 		String cumul = "";
-		for(Character chiffre:memoireCharacter)
+		for(Character chiffre:memoireCaractere)
 			cumul += chiffre;
 		Double oldNumber=pile.lastElement();
 		pile.push(Double.parseDouble(cumul));
 		pcs.firePropertyChange("saisie", null, Double.parseDouble(cumul));
 		pcs.firePropertyChange(PUSH, oldNumber, Double.parseDouble(cumul));
-		memoireCharacter = new ArrayList<Character>();
+		memoireCaractere = new ArrayList<Character>();
 	}
 
 	
